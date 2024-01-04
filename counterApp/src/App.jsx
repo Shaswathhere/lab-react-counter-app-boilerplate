2d110749db45aa67e6e39a5e1ea3b23dcaadedca
Shaswath <shaswath.g@kalvium.community>
2024-01-04 18:29:43 +0530

    done

---
+++ counterApp/src/App.jsx
@@ -0,0 +1,13 @@
+import CounterComponent from './Components/Component'
+import './App.css'
+
+function App() {
+
+  return(
+    <div>
+      <CounterComponent/>
+    </div>
+  )
+}
+
+export default App
